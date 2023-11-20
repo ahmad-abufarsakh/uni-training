c3ea53f265c231de85f52c1b637aa0f833ae96e9
ahmadabufarsakh <99365674+Farsakh@users.noreply.github.com>
2023-11-20 18:38:43 +0300

    Adding UniTrainingWeb Project

---
+++ UniTrainingWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UniTrainingWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
